0a0000f25a7dba5978ea1fdfd0a8aa817f9c6367
Yosh <gonzolezu@gmail.com>
2023-08-10 13:58:25 -0400

    created Socket routes

---
+++ src/routes/WebSocketRoutes.kt
@@ -0,0 +1,48 @@
+package com.plcoding.routes
+
+import com.google.gson.JsonParser
+import com.plcoding.data.models.BaseModel
+import com.plcoding.data.models.ChatMessage
+import com.plcoding.gson
+import com.plcoding.session.DrawingSession
+import com.plcoding.util.Constants.TYPE_CHAT_MESSAGE
+import io.ktor.http.cio.websocket.*
+import io.ktor.routing.*
+import io.ktor.sessions.*
+import io.ktor.websocket.*
+import kotlinx.coroutines.channels.consumeEach
+
+fun Route.standardWebSocket(
+    handleFrame: suspend (
+        socket: DefaultWebSocketServerSession,
+        clientId: String,
+        message: String,
+        payload: BaseModel
+    ) -> Unit
+) {
+    webSocket {
+        val session = call.sessions.get<DrawingSession>()
+        if (session == null) {
+            close(CloseReason(CloseReason.Codes.VIOLATED_POLICY, "No session"))
+            return@webSocket
+        }
+        try {
+            incoming.consumeEach { frame ->
+                if (frame is Frame.Text) {
+                    val message = frame.readText()
+                    val jsonObject = JsonParser.parseString(message).asJsonObject
+                    val type = when (jsonObject.get("type").asString) {
+                        TYPE_CHAT_MESSAGE -> ChatMessage::class.java
+                        else -> BaseModel::class.java
+                    }
+                    val payload = gson.fromJson(message, type)
+                    handleFrame(this, session.clientId, message, payload)
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            // handle logic when player disconnects?
+        }
+    }
+}
