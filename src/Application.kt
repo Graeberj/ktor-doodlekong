46b701257949e52715a9d6ed552c924016f52227
Yosh <gonzolezu@gmail.com>
2023-08-10 13:16:49 -0400

    added search functionality for rooms

--- src/Application.kt
+++ src/Application.kt
@@ -1,6 +1,7 @@
 package com.plcoding
 
 import com.plcoding.routes.createRoomRoute
+import com.plcoding.routes.getRoomsRoute
 import com.plcoding.session.DrawingSession
 import io.ktor.application.*
 import io.ktor.response.*
@@ -43,6 +44,7 @@ fun Application.module(testing: Boolean = false) {
 
     install(Routing){
         createRoomRoute()
+        getRoomsRoute()
     }
 }
 
