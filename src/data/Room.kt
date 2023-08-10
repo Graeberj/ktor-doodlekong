3a66994219de71eeebc8ede08a84771ed4ecb2fd
Yosh <gonzolezu@gmail.com>
2023-08-10 14:14:38 -0400

    handled game phase changes

--- src/data/Room.kt
+++ src/data/Room.kt
@@ -8,6 +8,33 @@ class Room(
     val maxPlayers: Int,
     var players: List<Player> = listOf()
 ) {
+
+    private var phaseChangedListener: ((Phase) -> Unit)? = null
+    var phase = Phase.WAITING_FOR_PLAYERS
+        set(value){
+            synchronized(field){
+                field = value
+                phaseChangedListener?.let { change ->
+                    change(value)
+                }
+            }
+        }
+
+    private fun setPhaseChangedListener(listener: (Phase) -> Unit){
+        phaseChangedListener = listener
+    }
+
+    init {
+        setPhaseChangedListener {phase ->
+            when(phase) {
+                Phase.WAITING_FOR_PLAYERS -> waitingForPlayers()
+                Phase.WAITING_TO_START -> waitingToStart()
+                Phase.NEW_ROUND -> newRound()
+                Phase.GAME_RUNNING -> gameRunning()
+                Phase.SHOW_WORD -> showWord()
+            }
+        }
+    }
     suspend fun broadcast(message: String){
         players.forEach{player ->
             if (player.socket.isActive){
@@ -26,5 +53,30 @@ class Room(
 
     fun containsPlayer(username: String): Boolean{
         return players.find { it.username == username } != null
+    }
+
+    private fun waitingForPlayers(){
+
+    }
+    private fun waitingToStart(){
+
+    }
+
+    private fun newRound(){
+
+    }
+    private fun gameRunning(){
+
+    }
+
+    private fun showWord(){
+
+    }
+    enum class Phase {
+        WAITING_FOR_PLAYERS,
+        WAITING_TO_START,
+        NEW_ROUND,
+        GAME_RUNNING,
+        SHOW_WORD
     }
 }
