b01b880d1a8f7255510eef5ee957a581428a533e
Yosh <gonzolezu@gmail.com>
2023-08-10 22:37:46 -0400

    broadcasting the drawdata

--- src/util/Constants.kt
+++ src/util/Constants.kt
@@ -3,5 +3,7 @@ package com.plcoding.util
 object Constants {
 
     const val MAX_ROOM_SIZE = 8
+
     const val TYPE_CHAT_MESSAGE = "TYPE_CHAT_MESSAGE"
+    const val TYPE_DRAW_DATA = "TYPE_DRAW_DATA"
 }
